5cc684e8d7f53655ad6f945bee4fbd284cf49be6
fattalterrin1 <fattalaaron01@gmail.com>
2025-07-23 12:01:17 -0700

    Update test.py

--- test class/test.py
+++ test class/test.py
@@ -2,6 +2,13 @@ print("hello")
 print("aaron")
 
 
+x=int(input("enter the number"))
+y=int(input("enter the number"))
+print("addition is",x+y)
+print("subtraction is",x-y)
+print("multiplcation is",x*y)
+print("divition is",x/y)
+print("remander is",x%y)
 
 
 # 1. git add .
