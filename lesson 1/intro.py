1a3a674104e3bf9d531b6a537aefe780434c4779
fattalterrin1 <fattalaaron01@gmail.com>
2025-07-23 12:01:03 -0700

    Update intro.py

--- lesson 1/intro.py
+++ lesson 1/intro.py
@@ -1,1 +1,8 @@
 print("this is intro class")
+x=int(input("enter the number"))
+y=int(input("enter the number"))
+print("addition is",x+y)
+print("subtraction is",x-y)
+print("multiplcation is",x*y)
+print("divition is",x/y)
+print("remander is",x%y)
